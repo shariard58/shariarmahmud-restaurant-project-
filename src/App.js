4fbe958ba705197923be28ea45b86c493e05213d
shariard58 <shariard58@gmail.com>
2022-05-13 01:54:22 -0600

    Component trees are ready

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
+// import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import Table from "react-bootstrap/Table";
+import MainComponent from "./components/MainComponent";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1> Shariar Mahmud Duke .. please try your best efforts </h1>
-
-      <button className="btn btn-success  custombtn"> Duke </button>
-
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="/Assests/twitter.jpg" />
-        <Card.Body>
-          <Card.Title>React Card </Card.Title>
-          <Card.Text> Shariar mahmud Duke </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
+      <MainComponent />
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>body</div>;
+};
+
+export default Header;
